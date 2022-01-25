7a14f700e840e207e2b82ee5cc8ebd3b6635c676
Denys Lytviak <deh4er@gmail.com>
2022-01-25 23:13:52 +0100

    change router to hashrouter

--- src/renderer/src/RootRouter.jsx
+++ src/renderer/src/RootRouter.jsx
@@ -2,12 +2,12 @@ import Editor from "@/editor/Editor";
 import Welcome from "@/welcome/Welcome";
 import { connect } from "react-redux";
 import { Navigate } from "react-router";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { selectTranslations } from "./editor/editorSlice";
 
 function RootRouter({ translations }) {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Welcome />}></Route>
         <Route
@@ -15,7 +15,7 @@ function RootRouter({ translations }) {
           element={translations.length > 0 ? <Editor /> : <Navigate to="/" />}
         ></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

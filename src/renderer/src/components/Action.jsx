336515ca4db0b4f829ffa8d23d2b595654b4f4fd
Denys Lytviak <deh4er@gmail.com>
2022-01-26 17:37:05 +0100

    remove unused components

--- src/renderer/src/components/Action.jsx
+++
@@ -1,9 +0,0 @@
-import { Button, KIND } from "baseui/button";
-
-export default function Action({ label, onClick }) {
-  return (
-    <Button kind={KIND.secondary} onClick={onClick}>
-      {label}
-    </Button>
-  );
-}
